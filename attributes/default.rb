7f0e27372fcc8a431b269569e72a7a9fdefb2068
sawanoboly <sawanoboriyu@higanworks.com>
2013-04-04 04:32:31 +0000

    support icmp

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 # Ranges:
 # populate the inbound_public_ranges attribute with a 
 # newline-delimited array of ips
+default['ipf']['pass_icmp']="any"
 default['ipf']['pass_in']=""
 default['ipf']['pass_out']=""
 default['ipf']['block_in']=""
