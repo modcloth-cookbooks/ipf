5731fe6e65ff543e619b40098add05a378d7d7a4
sawanoboly <sawanoboriyu@higanworks.com>
2013-04-17 15:48:39 +0900

    need depends using metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "b.irvin@modcloth.com"
 license          "Apache"
 description      "Installs/Configures ipf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4.0"
+version          "0.4.1"
 
-# depends          "smartmachine_functions"
+depends          "smartmachine_functions"
 supports         "smartos"
