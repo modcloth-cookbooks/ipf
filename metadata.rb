9f6a57fa8e0333ea4b4926e45d6f97d99ba7327c
Dan Buch <d.buch@modcloth.com>
2013-04-22 08:59:52 -0400

    Lots of cleanups I noticed while reviewing another PR

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "ModCloth, Inc."
-maintainer_email "b.irvin@modcloth.com"
+maintainer_email "platformsphere+ipf-cookbook@modcloth.com"
 license          "Apache"
 description      "Installs/Configures ipf"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.1"
 
 depends          "smartmachine_functions"
